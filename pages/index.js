5a621595bb260cc42caea19af406dbaf5a360be7
Kaourantinduplenne <kaourantin.duplenne@gmail.com>
2025-05-29 13:16:00 -0500

    Update index.js
    Removed "Rig Job By Design" on top of the page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ const RigJBDBuilder = dynamic(() => import('../components/RigJBDBuilder'), { ssr
 export default function Home() {
   return (
     <main className="p-4 max-w-5xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Rig Job By Design</h1>
       <RigJBDBuilder />
     </main>
   );
